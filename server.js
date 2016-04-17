fdf87345ebb2317c495dc90dd526830ef714ea13
NasawangD <Nasawangd@gmail.com>
2016-04-16 20:20:02 -0700

    added bootstrap and jquery packages, jquery is still not loading, therefore server wont run yet.

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
+var path = require('path');
+var bootstrap = require('bootstrap');
+
+
+//routes
+// app.use ('/login', require('./controllers/login.controllers'));
+
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// test route
+app.get('/myaccount', function(req, res) {
+ res.render("index");
+})
+
+//test routes
+app.get('/myaccount', function(res, req){
+    res.render('./views/index.ejs');
+});
 
 var server = app.listen(9000, function () {
     console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
